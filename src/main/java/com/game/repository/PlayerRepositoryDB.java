1eeeef6ff05f76933eb0b517841cc3869f3709d7
AlexandrKhalilulin <akhalilulin@mail.ru>
2022-11-07 12:53:17 +0300

    realize methods PlayerRepositoryDB

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,9 +1,12 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
@@ -31,36 +34,61 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try (Session session = sessionFactory.openSession()){
+            NativeQuery<Player> nativeQuery = session.createNativeQuery("select * from rpg.player", Player.class);
+            nativeQuery.setFirstResult(pageNumber*pageSize);
+            nativeQuery.setMaxResults(pageSize);
+            return nativeQuery.list();        }
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        try(Session session = sessionFactory.openSession()){
+            Query<Long> player_getAllCount = session.createNamedQuery("player_getAllCount", Long.class);
+            return Math.toIntExact(player_getAllCount.uniqueResult());
+        }
+
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.save(player);
+            session.getTransaction().commit();
+            return player;
+        }
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.update(player);
+            session.getTransaction().commit();
+            return player;
+        }
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        try(Session session = sessionFactory.openSession()){
+            Player player = session.find(Player.class, id);
+            return Optional.of(player);
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.remove(player);
+            session.getTransaction().commit();
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
